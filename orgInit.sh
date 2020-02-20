637afb49baa0636401cc89cd8140b0e84fd6fcd5
esandoval30 <esandoval@salesforce.com>
2020-02-20 18:26:54 +0000

    fixing iv.out location

--- orgInit.sh
+++ orgInit.sh
@@ -7,7 +7,7 @@ cat hc.out | awk 'FNR==2{ print substr($5,1,length($5)-1) }' > EVL_PEM_PROPERTY.
 heroku config:get $(cat EVL_EMAIL.PROPERTY.name) -a einstein-vl-emea > EVL_EMAIL.PROPERTY.value
 heroku config:get $(cat EVL_PEM_PROPERTY.name) -a einstein-vl-emea > unencrypted.pem
 openssl enc -nosalt -aes-128-cbc -in unencrypted.pem -out encrypted.pem -K 90E80538F0D659F08520E86DCA86F05F -iv B8DC0A25FC6C25F568F83EF5B951FA23
-cat iv.out encrypted.pem > einstein_platform.pem
+cat sec/iv.out encrypted.pem > einstein_platform.pem
 sfdx shane:ai:playground:setup -f einstein_platform.pem -e $(cat EVL_EMAIL.PROPERTY.value) -k kOgFOPDWWfCFIOhtyobwXw==
 sfdx force:org:open -p /lightning/setup/SetupOneHome/home
 rm hc.out unencrypted.pem iv.out encrypted.pem einstein_platform.pem EVL_*
