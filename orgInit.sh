a494ca8816207d647a618398735e229229dfbf09
esandoval30 <esandoval@salesforce.com>
2020-02-20 18:07:56 +0000

    slightly different syntax on echo command.

--- orgInit.sh
+++ orgInit.sh
@@ -6,7 +6,7 @@ cat hc.out | awk 'FNR==2{ print substr($4,1,length($4)-1) }' > EVL_EMAIL.PROPERT
 cat hc.out | awk 'FNR==2{ print substr($5,1,length($5)-1) }' > EVL_PEM_PROPERTY.name
 heroku config:get $(cat EVL_EMAIL.PROPERTY.name) -a einstein-vl-emea > EVL_EMAIL.PROPERTY.value
 heroku config:get $(cat EVL_PEM_PROPERTY.name) -a einstein-vl-emea > unencrypted.pem
-echo -n B8DC0A25FC6C25F568F83EF5B951FA23 | xxd -r -p - iv.out
+echo -n B8DC0A25FC6C25F568F83EF5B951FA23 | xxd -r -p > iv.out
 openssl enc -nosalt -aes-128-cbc -in unencrypted.pem -out encrypted.pem -K 90E80538F0D659F08520E86DCA86F05F -iv B8DC0A25FC6C25F568F83EF5B951FA23
 cat iv.out encrypted.pem > einstein_platform.pem
 sfdx shane:ai:playground:setup -f einstein_platform.pem -e $(cat EVL_EMAIL.PROPERTY.value) -k kOgFOPDWWfCFIOhtyobwXw==
