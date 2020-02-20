ff90affb97eaa2fe497f2f63576b8cadc6815723
esandoval30 <esandoval@salesforce.com>
2020-02-20 14:19:01 +0000

    testing heroku evl provisioning

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,5 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 sfdx force:source:push
 sfdx force:user:password:generate
+heroku addons:create einstein-vision:starter -a einstein-vl-emea
 sfdx force:org:open -p /lightning/setup/SetupOneHome/home
