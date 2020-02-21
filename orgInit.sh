d5d207954e37496a63c537ff0a32063665ca4070
esandoval30 <esandoval@salesforce.com>
2020-02-21 13:31:11 +0000

    debbuging

--- orgInit.sh
+++ orgInit.sh
@@ -5,7 +5,7 @@ heroku addons:create einstein-vision:starter -a einstein-vl-emea > hc.out
 cat hc.out | awk 'FNR==2{printf substr($4,1,length($4)-1)}'
 cat hc.out | awk 'FNR==2{printf substr($4,1,length($4)-1)}' > EVL_EMAIL_PROPERTY.name
 #cat hc.out | awk 'FNR==2{printf substr($5,1,length($5)-1)}' > EVL_PEM_PROPERTY.name
-heroku config:get $(cat EVL_EMAIL_PROPERTY.name) -a einstein-vl-emea
+heroku config:get $(cat hc.out | awk 'FNR==2{printf substr($4,1,length($4)-1)}') -a einstein-vl-emea
 #heroku config:get $(cat EVL_EMAIL_PROPERTY.name) -a einstein-vl-emea > EVL_EMAIL_PROPERTY.value
 #heroku config:get $(cat EVL_PEM_PROPERTY.name) -a einstein-vl-emea > unencrypted.pem
 #openssl enc -nosalt -aes-128-cbc -in unencrypted.pem -out encrypted.pem -K 90E80538F0D659F08520E86DCA86F05F -iv B8DC0A25FC6C25F568F83EF5B951FA23
