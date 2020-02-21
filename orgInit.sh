6c499887f27033fcabfa60bb3c89a6085e8abcd8
esandoval30 <esandoval@salesforce.com>
2020-02-21 14:27:44 +0000

    single heroku:config get

--- orgInit.sh
+++ orgInit.sh
@@ -1,12 +1,13 @@
+heroku config:get "EINSTEIN_VISION_GREEN_ACCOUNT_ID" -a einstein-vl-emea
 #sfdx force:org:create -f config/project-scratch-def.json -d 7 -s -w 60
 #sfdx force:source:push
 #sfdx force:user:password:generate
-heroku addons:create einstein-vision:starter -a einstein-vl-emea > hc.out
-cat hc.out | awk 'FNR==2{printf substr($4,1,length($4)-1)}'
-cat hc.out | awk 'FNR==2{printf substr($4,1,length($4)-1)}' > EVL_EMAIL_PROPERTY.name
+#heroku addons:create einstein-vision:starter -a einstein-vl-emea > hc.out
+#cat hc.out | awk 'FNR==2{printf substr($4,1,length($4)-1)}'
+#cat hc.out | awk 'FNR==2{printf substr($4,1,length($4)-1)}' > EVL_EMAIL_PROPERTY.name
 #cat hc.out | awk 'FNR==2{printf substr($5,1,length($5)-1)}' > EVL_PEM_PROPERTY.name
-cat EVL_EMAIL_PROPERTY.name
-heroku config:get $(cat EVL_EMAIL_PROPERTY.name) -a einstein-vl-emea
+#cat EVL_EMAIL_PROPERTY.name
+#heroku config:get $(cat EVL_EMAIL_PROPERTY.name) -a einstein-vl-emea
 #heroku config:get $(cat EVL_EMAIL_PROPERTY.name) -a einstein-vl-emea > EVL_EMAIL_PROPERTY.value
 #heroku config:get $(cat EVL_PEM_PROPERTY.name) -a einstein-vl-emea > unencrypted.pem
 #openssl enc -nosalt -aes-128-cbc -in unencrypted.pem -out encrypted.pem -K 90E80538F0D659F08520E86DCA86F05F -iv B8DC0A25FC6C25F568F83EF5B951FA23
