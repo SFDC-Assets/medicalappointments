9a0e154fa94edfdeb004347e05f24ed6661166b1
esandoval30 <esandoval@salesforce.com>
2020-02-28 10:23:25 +0000

    debugging herokuapp name

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,7 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 sfdx force:source:push
 sfdx force:user:password:generate
+basename "${PWD/mshanemc-/}"
 sfdx shane:heroku:repo:deploy -g mshanemc -r heroku-empty -n `basename "${PWD/mshanemc-/}"` -t autodeployed-demos
 sfdx shane:ai:playground:setupHeroku -a `basename "${PWD/mshanemc-/}"` -k
 sfdx force:org:open -p /lightning/setup/SetupOneHome/home
