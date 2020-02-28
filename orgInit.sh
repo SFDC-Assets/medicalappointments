aa18643080128337c88719afc4c35fade155df2a
esandoval30 <esandoval@salesforce.com>
2020-02-28 11:00:35 +0000

    shortening app name

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,7 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 sfdx force:source:push
 sfdx force:user:password:generate
-basename "${PWD/mshanemc-/}"
-sfdx shane:heroku:repo:deploy -g mshanemc -r heroku-empty -n `basename "${PWD/mshanemc-/}"` -t autodeployed-demos
-sfdx shane:ai:playground:setupHeroku -a `basename "${PWD/mshanemc-/}"` -k
+basename "${PWD/mshanemc-}" | awk -F'-' '{print "medical-appt-" $3}'
+sfdx shane:heroku:repo:deploy -g mshanemc -r heroku-empty -n `basename "${PWD/mshanemc-}" | awk -F'-' '{print "medical-appt-" $3}'` -t autodeployed-demos
+sfdx shane:ai:playground:setupHeroku -a `basename "${PWD/mshanemc-}" | awk -F'-' '{print "medical-appt-" $3}'` -k
 sfdx force:org:open -p /lightning/setup/SetupOneHome/home
