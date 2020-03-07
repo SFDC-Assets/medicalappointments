52c17e9679308f5fa41517724974ff31aa2ccc8d
esandoval30 <esandoval@salesforce.com>
2020-03-07 05:32:28 +0000

    Temporarily removing EVL account

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,7 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
+sfdx force:org:create -f config/project-scratch-def.json -d 1 -s -w 60
 sfdx force:source:push
 sfdx force:user:password:generate
-basename "${PWD/mshanemc-}" | awk -F'-' '{print "medical-appt-" $3}'
-sfdx shane:heroku:repo:deploy -g mshanemc -r heroku-empty -n `basename "${PWD/mshanemc-}" | awk -F'-' '{print "medical-appt-" $3}'` -t autodeployed-demos
-sfdx shane:ai:playground:setupHeroku -a `basename "${PWD/mshanemc-}" | awk -F'-' '{print "medical-appt-" $3}'` -k
+#basename "${PWD/mshanemc-}" | awk -F'-' '{print "medical-appt-" $3}'
+#sfdx shane:heroku:repo:deploy -g mshanemc -r heroku-empty -n `basename "${PWD/mshanemc-}" | awk -F'-' '{print "medical-appt-" $3}'` -t autodeployed-demos
+#sfdx shane:ai:playground:setupHeroku -a `basename "${PWD/mshanemc-}" | awk -F'-' '{print "medical-appt-" $3}'` -k
 sfdx force:org:open -p /lightning/setup/SetupOneHome/home
