45e78ffaa9d4f516a87ce15d203ad6f6afa6e955
esandoval30 <esandoval@salesforce.com>
2020-03-27 08:12:30 +0000

    Removing Vision & language stuff

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,4 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s -w 60
 sfdx force:source:push
 sfdx force:user:password:generate
-basename "${PWD/mshanemc-}" | awk -F'-' '{print "medical-appt-" $3}'
-sfdx shane:heroku:repo:deploy -g mshanemc -r heroku-empty -n `basename "${PWD/mshanemc-}" | awk -F'-' '{print "medical-appt-" $3}'` -t autodeployed-demos
-sfdx shane:ai:playground:setupHeroku --verbose -a `basename "${PWD/mshanemc-}" | awk -F'-' '{print "medical-appt-" $3}'` -k 
 sfdx force:org:open -p /lightning/setup/SetupOneHome/home
