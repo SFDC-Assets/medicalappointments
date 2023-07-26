c2bd802211102b63e093b02fb28975d44cf75e5f
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2023-07-25 20:37:56 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
+sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix medical -o appointments.workshop
 sfdx force:source:push
-sfdx force:user:password:generate
+sfdx shane:user:password:set -g User -l User -p salesforce1
 sfdx force:org:open -p /lightning/setup/SetupOneHome/home
